38232ac0f5827d7427ca9b4a58c20498e5cb366c
zuiidea <zuiiidea@gmail.com>
2017-06-22 00:50:00 +0800

    button

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import classnames from 'classnames'
 import { connect } from 'dva'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import FlatButton from 'material-ui/FlatButton'
+// import FlatButton from 'material-ui/FlatButton'
+import FlatButton from 'material-ui/RaisedButton'
 import Swiper from 'swiper'
 import styles from './index.less'
 import ListItem from './ListItem'
@@ -14,6 +15,7 @@ class Home extends React.Component {
       slidesPerView: 'auto',
       paginationClickable: true,
       spaceBetween: 0,
+      // centeredSlides: true,
     })
 
     this.contentSwiper = new Swiper(`.${styles.contentContainer}`, {
@@ -44,40 +46,45 @@ class Home extends React.Component {
   render() {
     const { home } = this.props
     const { list, index } = home
-    const { handleMenuItemClick, contentSwiper } = this
+    const { handleMenuItemClick, contentSwiper, menuSwiper } = this
 
     if (contentSwiper) {
       contentSwiper.slideTo(index, 500, false)
+    }
+    if (menuSwiper) {
+      menuSwiper.slideTo(index, 500, false)
     }
 
     const FlatButtonStyle = {
       height: `${lib.flexible.px2rem(28)}rem`,
       lineHeight: `${lib.flexible.px2rem(28)}rem`,
       minWidth: `${lib.flexible.px2rem(48)}rem`,
+      boxShadow: 'none',
       overflow: 'visible',
     }
 
     const labelStyle = {
-      fontSize: `${lib.flexible.px2rem(13)}rem`,
-      paddingLeft: `${lib.flexible.px2rem(16)}rem`,
-      paddingRight: `${lib.flexible.px2rem(16)}rem`,
-      verticalAlign: 'baseline',
+      fontSize: `${lib.flexible.px2rem(12)}rem`,
       fontWeight: 'normal',
+    }
+
+    const overlayStyle = {
+      lineHeight: `${lib.flexible.px2rem(28)}rem`,
     }
 
     return (
       <MuiThemeProvider>
         <div className={styles.home}>
           <div className={classnames({ 'swiper-container': true, [styles.menuContainer]: true })}>
-            <div className={classnames({ 'swiper-wrapper': true, [styles.menuWrapper]: true })}>
+            <div className={classnames({ 'swiper-wrapper': true })}>
               {
-                Array.from({ length: 6 }).map((item, key) => (
+                Array.from({ length: 8 }).map((item, key) => (
                   <span
                     key={key}
                     onClick={handleMenuItemClick.bind(null, key)}
                     className={classnames({ 'swiper-slide': true, [styles.menuSlide]: true, [styles.menuSlideActive]: key === index })}
                   >
-                    <FlatButton label={`按钮${key}`} style={FlatButtonStyle} labelStyle={labelStyle} />
+                    <FlatButton label={`按钮${key}`} style={FlatButtonStyle} labelStyle={labelStyle} overlayStyle={overlayStyle} />
                   </span>
                 ))
               }
@@ -87,7 +94,7 @@ class Home extends React.Component {
             <div className={classnames({ 'swiper-container': true, [styles.contentContainer]: true })} >
               <div className="swiper-wrapper">
                 {
-                    Array.from({ length: 6 }).map((item, key) => (
+                    Array.from({ length: 8 }).map((item, key) => (
                       <div label={`选项${key}`} key={key} className={classnames({ 'swiper-slide': true, [styles.contentSlide]: true })}>
                         {
                           list.map((iitem, iindex) => <ListItem key={iindex} data={iitem} />)
