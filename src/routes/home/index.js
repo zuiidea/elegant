a5cd3c094a6025e8e7aee33422e3d320f4eea1a4
zuiidea <zuiiidea@gmail.com>
2017-06-22 00:01:53 +0800

    swiper

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,87 +1,108 @@
 import React from 'react'
+import classnames from 'classnames'
 import { connect } from 'dva'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import FlatButton from 'material-ui/FlatButton'
-import Swiper from 'react-id-swiper'
+import Swiper from 'swiper'
 import styles from './index.less'
 import ListItem from './ListItem'
 
-let menuSwiper = {}
-let contentSwiper = {}
+class Home extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+    this.menuSwiper = new Swiper(`.${styles.menuContainer}`, {
+      slidesPerView: 'auto',
+      paginationClickable: true,
+      spaceBetween: 0,
+    })
 
-const Home = ({ loading, dispatch, location, home }) => {
-  // const { list, pagination, index } = home
-  // const { limit, offset, total } = pagination
-  // const { query } = location
-
-  const menuProps = {
-    spaceBetween: 0,
-    initialSlide: 2,
-    runCallbacksOnInit: true,
-    onInit: (swiper) => {
-      menuSwiper = swiper
-    },
+    this.contentSwiper = new Swiper(`.${styles.contentContainer}`, {
+      slidesPerView: 1,
+      paginationClickable: true,
+      spaceBetween: 0,
+      onSlideChangeEnd: (swiper) => {
+        dispatch({
+          type: 'home/updateState',
+          payload: {
+            index: swiper.activeIndex,
+          },
+        })
+      },
+    })
   }
 
-  const contentProps = {
-    slidesPerView: 'auto',
-    onSlideChangeEnd: (swiper) => {
-      console.log(swiper)
-      menuSwiper.slideTo(swiper.activeIndex, 1000, false)
-    },
-    runCallbacksOnInit: true,
-    onInit: (swiper) => {
-      contentSwiper = swiper
-    },
+  handleMenuItemClick = (index) => {
+    const { dispatch } = this.props
+    dispatch({
+      type: 'home/updateState',
+      payload: {
+        index,
+      },
+    })
   }
 
-  const FlatButtonStyle = {
-    height: `${lib.flexible.px2rem(28)}rem`,
-    lineHeight: `${lib.flexible.px2rem(28)}rem`,
-    minWidth: `${lib.flexible.px2rem(48)}rem`,
-    overflow: 'visible',
-  }
+  render() {
+    const { home } = this.props
+    const { list, index } = home
+    const { handleMenuItemClick, contentSwiper } = this
 
-  const labelStyle = {
-    fontSize: `${lib.flexible.px2rem(13)}rem`,
-    paddingLeft: `${lib.flexible.px2rem(16)}rem`,
-    paddingRight: `${lib.flexible.px2rem(16)}rem`,
-    verticalAlign: 'baseline',
-    fontWeight: 'normal',
-  }
+    if (contentSwiper) {
+      contentSwiper.slideTo(index, 500, false)
+    }
 
-  console.log(menuSwiper)
+    const FlatButtonStyle = {
+      height: `${lib.flexible.px2rem(28)}rem`,
+      lineHeight: `${lib.flexible.px2rem(28)}rem`,
+      minWidth: `${lib.flexible.px2rem(48)}rem`,
+      overflow: 'visible',
+    }
 
-  return (
-    <MuiThemeProvider>
-      <div className={styles.home}>
-        <div className={styles.menu}>
-          <Swiper {...menuProps}>
-            {
-            Array.from({ length: 8 }).map((item, key) => (
-              <span key={key} ><FlatButton label={`按钮${key}`} style={FlatButtonStyle} labelStyle={labelStyle} /></span>
-            ))
-          }
-          </Swiper>
+    const labelStyle = {
+      fontSize: `${lib.flexible.px2rem(13)}rem`,
+      paddingLeft: `${lib.flexible.px2rem(16)}rem`,
+      paddingRight: `${lib.flexible.px2rem(16)}rem`,
+      verticalAlign: 'baseline',
+      fontWeight: 'normal',
+    }
+
+    return (
+      <MuiThemeProvider>
+        <div className={styles.home}>
+          <div className={classnames({ 'swiper-container': true, [styles.menuContainer]: true })}>
+            <div className={classnames({ 'swiper-wrapper': true, [styles.menuWrapper]: true })}>
+              {
+                Array.from({ length: 6 }).map((item, key) => (
+                  <span
+                    key={key}
+                    onClick={handleMenuItemClick.bind(null, key)}
+                    className={classnames({ 'swiper-slide': true, [styles.menuSlide]: true, [styles.menuSlideActive]: key === index })}
+                  >
+                    <FlatButton label={`按钮${key}`} style={FlatButtonStyle} labelStyle={labelStyle} />
+                  </span>
+                ))
+              }
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={classnames({ 'swiper-container': true, [styles.contentContainer]: true })} >
+              <div className="swiper-wrapper">
+                {
+                    Array.from({ length: 6 }).map((item, key) => (
+                      <div label={`选项${key}`} key={key} className={classnames({ 'swiper-slide': true, [styles.contentSlide]: true })}>
+                        {
+                          list.map((iitem, iindex) => <ListItem key={iindex} data={iitem} />)
+                        }
+                      </div>
+                    ))
+                  }
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.content}>
-          <Swiper {...contentProps} className={styles.slideContainer}>
-            {
-              Array.from({ length: 3 }).map((item, key) => (
-                <div label={`选项${key}`} key={key} >
-                  <div className={styles.slide}>
-                    {
-                      list.map((item, index) => <ListItem key={index} data={item} />)
-                    }
-                  </div>
-                </div>
-              ))
-            }
-          </Swiper>
-        </div>
-      </div>
-    </MuiThemeProvider>
-  )
+      </MuiThemeProvider>
+    )
+  }
 }
+
 
 export default connect(({ home, loading }) => ({ home, loading }))(Home)
